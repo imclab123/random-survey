ced7421637335ef13e52e2386fdc75df12372154
imclab123 <imc.lab.nccu@gmail.com>
2025-11-08 15:51:12 +0800

    Reformat URLs in url.js
    Reformatted URLs array for consistency.

--- url.js
+++ url.js
@@ -8,12 +8,12 @@
 
 
 const urls = [
-	'https://zh.surveymonkey.com/r/DGKS5CD'
-	'https://zh.surveymonkey.com/r/NZ25MKZ'
-    'https://zh.surveymonkey.com/r/DG27N2L'
-    'https://zh.surveymonkey.com/r/WFLTXHM'
-    'https://zh.surveymonkey.com/r/WFVRS9Z'
-    'https://zh.surveymonkey.com/r/WF7BY8L'
-    'https://zh.surveymonkey.com/r/DGW2ZD2'
+	'https://zh.surveymonkey.com/r/DGKS5CD',
+	'https://zh.surveymonkey.com/r/NZ25MKZ',
+    'https://zh.surveymonkey.com/r/DG27N2L',
+    'https://zh.surveymonkey.com/r/WFLTXHM',
+    'https://zh.surveymonkey.com/r/WFVRS9Z',
+    'https://zh.surveymonkey.com/r/WF7BY8L',
+    'https://zh.surveymonkey.com/r/DGW2ZD2',
     'https://zh.surveymonkey.com/r/DGS6NRD'
 	];
